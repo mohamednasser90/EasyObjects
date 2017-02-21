8ae49a1075246f3567b9fd2f67625200fd840f52
mgnoonan <noonanconsulting@outlook.com>
2017-02-20 21:40:29 -0500

    initial stab at getting a working console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string sql = @"
+SELECT [RegionID]
+      ,[RegionDescription]
+  FROM [Northwind].[dbo].[Region]";
+
+                var list = conn.Query<Region>(sql);
+
+                Console.WriteLine("ID\tDescription");
+                foreach (var l in list)
+                {
+                    Console.WriteLine("{0}\t{1}", l.RegionID, l.RegionDescription);
+                }
+            }
+
+            Console.WriteLine("Press <Enter> to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Region
+    {
+        [Key]
+        public int RegionID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RegionDescription { get; set; }
+    }
+}
